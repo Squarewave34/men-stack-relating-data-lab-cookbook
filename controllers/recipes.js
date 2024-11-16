3c345fb04ccebc85a471348785fd4fe9c678bb97
Squarewave34 <132561675+Squarewave34@users.noreply.github.com>
2024-11-17 00:58:48 +0300

    can show individual recipes now

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -6,7 +6,8 @@ const Ingredient = require('../models/ingredient')
 const router = express.Router()
 
 router.get('/', async(req, res)=>{
-  res.render('recipes/index.ejs')
+  const recipes = await Recipe.find()
+  res.render('recipes/index.ejs', {recipes})
 })
 
 router.get('/new', async(req, res)=>{
@@ -19,5 +20,11 @@ router.post('/', async (req, res) => {
   await Recipe.create(req.body);
   res.redirect('/');
 });
+
+router.get('/:recipeId', async(req, res)=>{
+  const id=req.params.recipeId
+  const recipe = await Recipe.findById(id)
+  res.render('recipes/show.ejs', {recipe: recipe})
+})
 
 module.exports = router
